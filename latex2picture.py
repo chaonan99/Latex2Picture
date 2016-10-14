4d524bdb6d2e5cfac6c4f12367ce5bf91dcb56c8
朝南 <chn13@hotmail.com>
2016-10-14 14:27:26 +0800

    Fix bug to support multi selection

--- latex2picture.py
+++ latex2picture.py
@@ -1,19 +1,6 @@
 import sublime, sublime_plugin, urllib
 
-def selections(view, default_to_all=True):
-    """
-    Return all non-empty selections in view
-    If None, return entire view if default_to_all is True
-    code from: https://github.com/mastahyeti/URLEncode
-    """
-    regions = [r for r in view.sel() if not r.empty()]
-
-    if not regions and default_to_all:
-        regions = [sublime.Region(0, view.size())]
-
-    return regions
-
-def quote(view, s):
+def quote(s):
     return "![equation](http://latex.codecogs.com/svg.latex?" + urllib.parse.quote(s, safe='') + ")"
 
 class LatexPictureCommand(sublime_plugin.TextCommand):
@@ -22,6 +9,9 @@ class LatexPictureCommand(sublime_plugin.TextCommand):
     """
     def run(self, edit):
         view = self.view
-        for region in selections(view):
-            s = view.substr(region)
-            view.replace(edit, region, quote(view, s))
+        s = []
+        for region in view.sel():
+            if not region.empty():
+                s = view.substr(region)
+                re = quote(s)
+                view.replace(edit, region, re)
